344d03c62f3312575b29065039f4acda68b7bd60
sahibzadakashif <kashifsahibzada88@gmail.com>
2025-01-05 12:05:26 +0500

    octa.py

--- octa.py
+++ octa.py
@@ -83,7 +83,7 @@ with st.container():  # Corrected from 'center' to 'st.container'
         """,
         unsafe_allow_html=True
     )
-    st.image("imagef.jpg", width=1000)
+    st.image("imagef.jpg", width=1200)
 
 
 
